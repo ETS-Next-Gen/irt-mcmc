ca04e6c77286123b3705090d0ac9dce5906c244e
Livne <olivne@M-AI813029-010044116056.etsclient.org>
2019-12-11 11:24:04 -0500

    Added low, high parameterization to 1D integration program.

--- code/integration/smooth_integration.py
+++ code/integration/smooth_integration.py
@@ -4,24 +4,29 @@ import numpy as np
 from scipy.integrate import fixed_quad, quadrature
 
 def theta_distribution(theta, mu, sigma):
+    """Latent ability distribution."""
     return np.exp(-(x-mu)**2 / sigma**2)
 
 def irf(theta, a, b):
+    """Item response function."""
     t = np.exp(a*(theta - b))
     return t / (1 + t)
 
 def likelihood(theta, a, b, mu, sigma):
     return irf(theta, a, b) * irf(theta, mu, sigma)
 
-if __name__ == "__main__":
+def test_integration_error(lo, high):
+    """Prints integration error of Gaussian quadratures for integrating the likelihood over [lo,high]."""
     a, b = 1, 1
     mu, sigma = 0, 1
-
-    print("Integrating likelihood function with item parameters a = {}, b = {}, theta = Normal({}, {})".format(a, b, mu, sigma))
-
-    s, error = quadrature(likelihood, -1, 6, args=(a, b, mu, sigma), tol=1e-08, rtol=1e-08, maxiter=50)
+    print("Integrating likelihood function with item parameters "
+          "a = {}, b = {}, theta = Normal({}, {}) over [{},{}]".format(a, b, mu, sigma, lo, high))
+    s, error = quadrature(likelihood, lo, high, args=(a, b, mu, sigma), tol=1e-08, rtol=1e-08, maxiter=50)
     print("Fixed-tolerance quadrature:      result = %.8f  error = %.2e" % (s, error))
-
     for n in range(5, 11):
-        sn, _ = fixed_quad(likelihood, -1, 6, args=(a, b, mu, sigma), n=n)
+        sn, _ = fixed_quad(likelihood, lo, high, args=(a, b, mu, sigma), n=n)
         print("Fixed-point quadrature: n = %2d   result = %.8f  error = %.2e" % (n, sn, np.abs(s - sn)))
+
+if __name__ == "__main__":
+    for lo in (-3, -2, -1, 0):
+        test_integration_error(lo, 6)
